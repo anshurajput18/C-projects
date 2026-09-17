dbde4aff340d6fe09ffabd576a2a061720f0a5f7
anshurajput18 <your.evilb24@gmail.com>
2026-09-17 23:03:45 +0530

    made the simple calculator

--- calculator.c
+++ calculator.c
@@ -1,22 +1,81 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(){
+void print_menu();
+int main()
+{
     int choice;
-    double first,second,result;
-    do
+    double first, second, result;
+    while (1)
     {
-        printf("Welcome to the calculator program\n");
-        printf("Please select an operation:\n");
-        printf("\n1. Addition\n");
-        printf("\n2. Subtraction\n");
-        printf("\n3. Multiplication\n");
-        printf("\n4. Division\n");
-        printf("\n5. Square Root\n");
-        printf("\n6. Power\n");
-        printf("\n7. Exit\n");
-        printf("Enter your choice: ");
+        print_menu();
         scanf("%d", &choice);
-    } while (choice != 6);
+        if (choice == 6)
+        {
+            printf("Exiting the program. Goodbye!\n");
+            break;
+        }
+        printf("first number :");
+        scanf("%lf", &first);
+        printf("second number : ");
+        scanf("%lf", &second);
+
+        switch (choice)
+        {
+        case 1:
+            result = first + second;
+            printf("Result: %.2lf\n", result);
+            break;
+        case 2:
+            result = first - second;
+            printf("Result: %.2lf\n", result);
+            break;
+        case 3:
+            result = first * second;
+            printf("Result: %.2lf\n", result);
+            break;
+        case 4:
+            if (second != 0)
+            {
+                result = first / second;
+                printf("Result: %.2lf\n", result);
+            }
+            else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        // case 5:
+        //     if (first >= 0)
+        //     {
+        //         result = sqrt(first);
+
+        //         printf("Result: %.2lf\n", result);
+        //     }
+        //     else
+        //     {
+        //         printf("Error: Square root of a negative number is not defined.\n");
+        //     }
+        //     break;
+        case 5:
+            result = pow(first, second);
+            printf("Result: %.2lf\n", result);
+            break;
+        }
+    }
+}
+void print_menu()
+{
+    printf("\n--------------------------------\n");
+    printf("Welcome to the calculator program\n");
+    printf("\nPlease select an operation:\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    // printf("5. Square Root\n");
+    printf("5. Power\n");
+    printf("6. Exit\n");
+    printf("Enter your choice: ");
 }
